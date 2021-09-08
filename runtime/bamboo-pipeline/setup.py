8a337d69132ad67d5343dafcdd2fc0b02f8cb8a4
homholueng <homholueng@gmail.com>
2021-09-08 16:04:45 +0800

    minor: runtime pipeline setup.py optimization

--- runtime/bamboo-pipeline/setup.py
+++ runtime/bamboo-pipeline/setup.py
@@ -18,11 +18,8 @@ version = __import__("pipeline").__version__
 
 setup(
     name="bamboo-pipeline",
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version=version,
-    description="pipeline",  # noqa
+    description="bamboo-pipeline",
     long_description="runtime for bamboo-engine base on Django and Celery",
     # The project's main homepage.
     url="https://github.com/TencentBlueKing/bamboo-engine/runtime/bamboo-pipeline",
@@ -30,16 +27,14 @@ setup(
     author="Blueking",
     author_email="contactus_bk@tencent.com",
     include_package_data=True,
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(),
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
+    packages=find_packages(
+        exclude=[
+            "pipeline.tests",
+            "pipeline.tests.*",
+            "test",
+            "test.*",
+        ],
+    ),
     install_requires=[
         # Base
         "Django>=2.2.6,<4.0",
@@ -62,10 +57,4 @@ setup(
         "prometheus-client>=0.9.0,<1.0",
     ],
     zip_safe=False,
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    # entry_points={
-    #     "console_scripts": ["bk-admin=blueapps.contrib.bk_commands:bk_admin"],
-    # },
 )
