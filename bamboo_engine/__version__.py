6fe87d872174fb03003e42d89287c7c8b6b63920
homholueng <homholueng@gmail.com>
2022-04-14 10:04:24 +0800

    minor: release bamboo-engine 2.2.1

--- bamboo_engine/__version__.py
+++ bamboo_engine/__version__.py
@@ -11,4 +11,4 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 
-__version__ = "2.2.0"
+__version__ = "2.2.1"
