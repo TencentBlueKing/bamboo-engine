ea2032b38a5b8f14ed0c3e9f853109fccc93dd76
homholueng <homholueng@gmail.com>
2022-05-23 11:39:04 +0800

    minor: remove useless import

--- runtime/bamboo-pipeline/pipeline/eri/celery/tasks.py
+++ runtime/bamboo-pipeline/pipeline/eri/celery/tasks.py
@@ -10,8 +10,6 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-import imp
-from multiprocessing.spawn import import_main_path
 import time
 import logging
 from typing import Optional
