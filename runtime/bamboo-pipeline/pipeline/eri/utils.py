ef0a6a4e5212b7fed7af82b67f92573a50ed8921
hanshuaikang <1758504262@qq.com>
2023-06-01 20:46:28 +0800

    feature: 新增check_worker函数检查worker状态

--- runtime/bamboo-pipeline/pipeline/eri/utils.py
+++ runtime/bamboo-pipeline/pipeline/eri/utils.py
@@ -10,9 +10,12 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-
+import socket
 from typing import Dict
+
 from bamboo_engine.eri import ContextValueType
+from celery import current_app
+from django.conf import settings
 
 CONTEXT_TYPE_MAP = {
     "plain": ContextValueType.PLAIN,
@@ -50,3 +53,25 @@ def caculate_final_references(original_references: Dict[str, set]) -> Dict[str, 
                 queue.extend(original_references[r])
 
     return final_references
+
+
+def check_worker(connection=None):
+    worker_list = []
+    tries = 0
+    WORKER_PING_TIMES = getattr(settings, "PIPELINE_WORKER_PING_TIMES", 2)
+    while tries < WORKER_PING_TIMES:
+        kwargs = {"timeout": tries + 1}
+        if connection is not None:
+            kwargs["connection"] = connection
+        try:
+            worker_list = current_app.control.ping(**kwargs)
+        except socket.error as err:
+            if tries >= WORKER_PING_TIMES - 1:
+                raise err
+
+        if worker_list:
+            break
+
+        tries += 1
+
+    return True, worker_list
