df05acb4cb3f46a02273caf6381e51f72f23b3a0
hanshuaikang <1758504262@qq.com>
2023-07-06 17:02:46 +0800

    test: 新增分支命中策略的单元测试

--- tests/handlers/test_exclusive_gateway.py
+++ tests/handlers/test_exclusive_gateway.py
@@ -23,6 +23,7 @@ from bamboo_engine.eri import (
     ProcessInfo,
 )
 from bamboo_engine.handlers.exclusive_gateway import ExclusiveGatewayHandler
+from bamboo_engine.utils.constants import ExclusiveGatewayStrategy, RuntimeSettings
 from bamboo_engine.utils.expr import default_expr_func
 
 
@@ -403,6 +404,75 @@ def test_exclusive_gateway__execute_success(recover_point):
     assert result.schedule_after == -1
     assert result.dispatch_processes == []
     assert result.next_node_id == "t3"
+    assert result.should_die == False
+
+    runtime.get_data_inputs.assert_called_once_with("root")
+    runtime.get_context_key_references.assert_called_once_with(pipeline_id=pi.top_pipeline_id, keys=set())
+    runtime.get_context_values.assert_called_once_with(pipeline_id=pi.top_pipeline_id, keys=set())
+    runtime.set_state.assert_called_once_with(
+        node_id=node.id,
+        version="v1",
+        to_state=states.FINISHED,
+        set_archive_time=True,
+        ignore_boring_set=recover_point is not None,
+    )
+
+
+@pytest.mark.parametrize(
+    "recover_point",
+    [
+        pytest.param(MagicMock(), id="recover_is_not_none"),
+        pytest.param(None, id="recover_is_none"),
+    ],
+)
+def test_exclusive_gateway_first_strategy_success(recover_point):
+    conditions = [
+        Condition(name="c1", evaluation="2 == 2", target_id="t1", flow_id="f1"),
+        Condition(name="c2", evaluation="0 == 1", target_id="t2", flow_id="f2"),
+        Condition(name="c3", evaluation="1 == 1", target_id="t3", flow_id="f3"),
+    ]
+    node = ExclusiveGateway(
+        conditions=conditions,
+        id="nid",
+        type=NodeType.ExclusiveGateway,
+        target_flows=["f1", "f2", "f3"],
+        target_nodes=["t1", "t2", "t3"],
+        targets={"f1": "t1", "f2": "t2", "f3": "t3"},
+        root_pipeline_id="root",
+        parent_pipeline_id="root",
+        can_skip=True,
+    )
+    pi = ProcessInfo(
+        process_id="pid",
+        destination_id="",
+        root_pipeline_id="root",
+        pipeline_stack=["root"],
+        parent_id="parent",
+    )
+    additional_refs = []
+
+    runtime = MagicMock()
+    runtime.get_context_key_references = MagicMock(return_value=additional_refs)
+    runtime.get_context_values = MagicMock(return_value=[])
+    runtime.get_data_inputs = MagicMock(return_value={})
+
+    def get_config(config_name):
+        if config_name == RuntimeSettings.EXCLUSIVE_GATEWAY_EXPR_FUNC.value:
+            return default_expr_func
+        if config_name == RuntimeSettings.EXCLUSIVE_GATEWAY_STRATEGY.value:
+            return ExclusiveGatewayStrategy.FIRST.value
+
+    runtime.get_config = MagicMock(side_effect=get_config)
+
+    handler = ExclusiveGatewayHandler(node, runtime, MagicMock)
+    result = handler.execute(pi, 1, 1, "v1", recover_point)
+
+    assert result.should_sleep == False
+    assert result.schedule_ready == False
+    assert result.schedule_type == None
+    assert result.schedule_after == -1
+    assert result.dispatch_processes == []
+    assert result.next_node_id == "t1"
     assert result.should_die == False
 
     runtime.get_data_inputs.assert_called_once_with("root")
